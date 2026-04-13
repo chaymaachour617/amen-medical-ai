f3d991dc17a1485c75fc5381ad49c59a1bb0d8c9
benmedikram <benmohamedik0507@gmail.com>
2026-04-13 15:28:32 +0100

    updating back

--- app/api/routes/session.py
+++ app/api/routes/session.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session as DBSession
 from app.db.database import SessionLocal
 from app.schemas.session import SessionCreate, SessionResponse
 from app.services import session_service
@@ -16,20 +16,20 @@ def get_db():
 
 
 @router.post("/", response_model=SessionResponse)
-def create_session(session: SessionCreate, db: Session = Depends(get_db)):
+def create_session(session: SessionCreate, db: DBSession = Depends(get_db)):
     return session_service.create_session(db, session.model_dump())
 
 
 @router.get("/", response_model=list[SessionResponse])
-def get_sessions(db: Session = Depends(get_db)):
+def get_sessions(db: DBSession = Depends(get_db)):
     return session_service.get_all_sessions(db)
 
 
 @router.get("/{session_id}", response_model=SessionResponse)
-def get_session(session_id: int, db: Session = Depends(get_db)):
+def get_session(session_id: int, db: DBSession = Depends(get_db)):
     return session_service.get_session_by_id(db, session_id)
 
 
 @router.delete("/{session_id}")
-def delete_session(session_id: int, db: Session = Depends(get_db)):
+def delete_session(session_id: int, db: DBSession = Depends(get_db)):
     return session_service.delete_session(db, session_id)
